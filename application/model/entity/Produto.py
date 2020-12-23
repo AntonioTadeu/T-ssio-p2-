a966d57f87939d2726f2fd9154f3e8fb9bc43b8d
Antônio Tadeu <antoniotadeubf98@gmail.com>
2020-12-23 19:27:21 -0300

    Feat: Produto
    correção dos atributos do produto.py

--- application/model/entity/Produto.py
+++ application/model/entity/Produto.py
@@ -1,15 +1,20 @@
 class Produto:
 
-    def __init__(self, id, nome, descricao, precoOficial, precoLancamento, parcelamento):
+    def __init__(self, id, imagemProduto,  nome, descricao, precoOficial, precoLancamento, valorParcela, numeroParcelas):
         self._id = id 
+        self._imagemProduto = imagemProduto
         self._nome = nome
         self._descricao = descricao
         self._precoOficial = precoOficial
         self._precoLancamento = precoLancamento 
-        self._parcelamento = parcelamento 
+        self._valorParcela = valorParcela
+        self._numeroParcelas = numeroParcelas
 
     def get_id(self):
         return self._id
+
+    def get_imagemProduto(self):
+        return self._imagemProduto
 
     def get_nome(self):
         return self._nome
@@ -23,8 +28,13 @@ class Produto:
     def get_precoLancamento(self):
         return self._precoLancamento
 
-    def get_parcelamento(self):
-        return self._parcelamento
+    def get_valorParcela(self):
+        return self._valorParcela
+
+    def get_numeroParcelas(self):
+        return self._numeroParcelas
+
+
 
 
 
